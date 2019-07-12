4322cd650a55c1244461fbdbe8661c9bf97281aa
alexandre-dias <alexandre-dias@outlook.com.br>
2019-07-12 11:01:19 -0300

    Janela principal criada

--- Code.py
+++ Code.py
@@ -1,2 +1,18 @@
 # Programa desenvolvido para aprimoramento de conhecimento na linguagem Python
 # Início do desenvolvimento: 12/07/2019
+
+# Importando biblioteca de interface gráfica
+import tkinter as tk
+
+# Criando a janela principal
+janela = tk.Tk()
+
+# Define um título para a janela
+janela.title("IMC")
+
+# Redimensionar a janela
+# Largura x Altura + Distância da Esquerda do vídeo + Distância do Topo do vídeo
+janela.geometry("300x300+100+100")
+
+# Comando para execução
+janela.mainloop()
